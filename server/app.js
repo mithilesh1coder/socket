d1ca01a021708937b794e8480e0860dc0f6b7b6f
MI396547 <mithilesh.pradhan@wipro.com>
2018-06-19 16:23:10 +0530

    add socket connection

--- server/app.js
+++ server/app.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const path = require('path');
+const http = require('http');
+const socketIO =require('socket.io');
 
 var app = express();
 var url = path.join(__dirname,'../public');
 const port = process.env.PORT || 4000 ;
 
+var server = http.createServer((app));
+
 app.use(express.static(url));
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Listening on port ${port}`);
+})
+
+var io = socketIO(server);
+
+io.on('connection',(socket) => {
+    console.log('user connected');
+
+    socket.on('disconnect',() => {
+        console.log('User disconnected');
+    })
 })
